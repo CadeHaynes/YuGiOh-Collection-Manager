96b2d1aa48452d2ca54d7164f2ba8e6149b585df
Laevahteinn <cade.haynes@gmail.com>
2026-08-18 18:57:35 +1000

    Database relationships and migrations set up

--- YGOCM_BACKEND/AppDbContext.cs
+++ YGOCM_BACKEND/AppDbContext.cs
@@ -1,11 +1,44 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using YGOCM_BACKEND.Entities;
+
 namespace YGOCM_BACKEND
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
+        }
+
+        // Entity Sets
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<CollectionEntry> CollectionEntries { get; set; }
+
+        // Create relations in the database
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // CollectionEntry has one User, with Users having many CollectionEntries
+            modelBuilder.Entity<CollectionEntry>()
+                .HasOne(c => c.User)
+                .WithMany(u => u.Collection)
+                .HasForeignKey(c => c.UserId);
+
+            // CollectionEntry has one Card, and Cards can be referenced by many CollectionEntries
+            modelBuilder.Entity<CollectionEntry>()
+                .HasOne(c => c.Card)
+                .WithMany()
+                .HasForeignKey(c => c.CardId);
+
+            // CollectionEntry has a unique CardId and UserId
+            modelBuilder.Entity<CollectionEntry>()
+                .HasIndex(c => new { c.CardId, c.UserId })
+                .IsUnique();
+
+            // CollectionEntry requires a valid Quanity
+            modelBuilder.Entity<CollectionEntry>()
+                .Property(c => c.Quantity)
+                .IsRequired();
         }
     }
 }
---
+++ YGOCM_BACKEND/Migrations/20260818085326_CreateBaseEntities.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace YGOCM_BACKEND.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateBaseEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    YgoProId = table.Column<string>(type: "text", nullable: false),
+                    CardType = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    MonsterType = table.Column<string>(type: "text", nullable: true),
+                    MonsterAttribute = table.Column<string>(type: "text", nullable: true),
+                    MonsterLevel = table.Column<int>(type: "integer", nullable: true),
+                    MonsterAttack = table.Column<int>(type: "integer", nullable: true),
+                    MonsterDefense = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CollectionEntries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Quantity = table.Column<int>(type: "integer", nullable: false),
+                    CardId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CollectionEntries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CollectionEntries_Cards_CardId",
+                        column: x => x.CardId,
+                        principalTable: "Cards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CollectionEntries_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionEntries_CardId_UserId",
+                table: "CollectionEntries",
+                columns: new[] { "CardId", "UserId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CollectionEntries_UserId",
+                table: "CollectionEntries",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CollectionEntries");
+
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
