f5a524b5189d9083dcb1fd4fb2ae1116dab91b6a
Laevahteinn <cade.haynes@gmail.com>
2026-08-18 19:16:54 +1000

    Added initial CardController to test previous changes, also updated launchSettings to use correct url.

---
+++ YGOCM_BACKEND/Controllers/CardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using YGOCM_BACKEND.Entities;
+
+namespace YGOCM_BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CardController : ControllerBase
+    {
+        // Database reference
+        readonly AppDbContext _context;
+
+        public CardController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // HTTP Calls
+        [HttpGet] // GET all cards in the database
+        public async Task<ActionResult<IEnumerable<Card>>> GetCards()
+        {
+            return await _context.Cards.ToListAsync();
+        }
+    }
+}
