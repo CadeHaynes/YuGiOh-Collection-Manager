1fc62ee79ff0bc0eac5844514ff30f8fb2a562d2
Laevahteinn <cade.haynes@gmail.com>
2026-08-25 14:40:31 +1000

    Ypd API and Card Db controllers separated

--- YGOCM_BACKEND/Controllers/CardController.cs
+++ YGOCM_BACKEND/Controllers/CardController.cs
@@ -2,23 +2,21 @@
 using Microsoft.EntityFrameworkCore;
 
 using YGOCM_BACKEND.Entities;
-using YGOCM_BACKEND.Services;
 using YGOCM_BACKEND.DTOs;
 
 namespace YGOCM_BACKEND.Controllers
 {
+    // This controller is for managing the cards in the database.
     [ApiController]
     [Route("api/[controller]")]
     public class CardController : ControllerBase
     {
         // Database reference
         readonly AppDbContext _context;
-        readonly YgoProDeckService _ypdService;
 
-        public CardController(AppDbContext context, YgoProDeckService ypdService)
+        public CardController(AppDbContext context)
         {
             _context = context;
-            _ypdService = ypdService;
         }
 
         // HTTP Calls
@@ -28,60 +26,19 @@ namespace YGOCM_BACKEND.Controllers
             return await _context.Cards.ToListAsync();
         }
 
-        [HttpGet("{id}")] // GET a specific card from the database by id
-        public async Task<ActionResult<Card?>> GetCardByIdFromDB(int id)
+        [HttpGet("id/{id}")] // GET a specific card from the database by id
+        public async Task<ActionResult<Card?>> GetCardById(int id)
         {
             return await _context.Cards.FindAsync(id);
         }
 
-        [HttpGet("/db/{name}")] // GET a specific card from the database by name
-        public async Task<ActionResult<Card?>> GetCardByNameFromDB(string name)
+        [HttpGet("name/{name}")] // GET a specific card from the database by name
+        public async Task<ActionResult<Card?>> GetCardByName(string name)
         {
             var card = _context.Cards.Any(c => c.Name == name);
 
             return await _context.Cards.FindAsync(name);
-        }
-
-        [HttpGet("/ypd/{name}")] // GET a specific card from the API by name, using the service
-        public async Task<ActionResult<YgoProDeckCard?>> GetCardByNameFromAPI(string name)
-        {
-            return await _ypdService.GetCardAsync(name);
-        }
-
-        [HttpPost] // POST a specific card from the API to the database
-        public async Task<ActionResult<YgoProDeckCard?>> PostCardFromAPI(string name)
-        {
-            var card = await _ypdService.GetCardAsync(name);
-
-            if (card == null)
-            {
-                return NotFound();
-            }
-
-            if (await _context.Cards.AnyAsync(c => c.Name == card.Name))
-            {
-                return BadRequest();
-            }
-
-            var dbCard = new Card
-            {
-                Name = card.Name,
-                YgoProId = card.Id,
-                CardType = card.Type,
-                MonsterType = card.Race,
-                MonsterAttribute = card.Attribute,
-                MonsterLevel = card.Level,
-                MonsterAttack = card.Atk,
-                MonsterDefense = card.Def,
-                Description = card.Desc
-                //Image = card.Image
-            };
-
-            _context.Cards.Add(dbCard);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetCardByIdFromDB), new { id = dbCard.Id }, dbCard);
-        }
+        } 
 
         [HttpDelete] // DELETE all cards from the database
         public async Task<IActionResult> ClearDb()
---
+++ YGOCM_BACKEND/Controllers/YpdController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using YGOCM_BACKEND.Entities;
+using YGOCM_BACKEND.Services;
+using YGOCM_BACKEND.DTOs;
+
+namespace YGOCM_BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class YpdController : ControllerBase
+    {
+        // Database reference
+        readonly AppDbContext _context;
+        readonly YgoProDeckService _ypdService;
+
+        public YpdController(AppDbContext context, YgoProDeckService ypdService)
+        {
+            _context = context;
+            _ypdService = ypdService;
+        }
+
+        // HTTP Calls
+        [HttpGet("{name}")] // GET a specific card from the API by name, using the service
+        public async Task<ActionResult<YgoProDeckCard?>> GetCardByNameFromAPI(string name)
+        {
+            return await _ypdService.GetCardAsync(name);
+        }
+
+        [HttpPost("{name}")] // POST a specific card from the API to the database
+        public async Task<ActionResult<YgoProDeckCard?>> PostCardFromAPI(string name)
+        {
+            var card = await _ypdService.GetCardAsync(name);
+
+            if (card == null)
+            {
+                return NotFound();
+            }
+
+            if (await _context.Cards.AnyAsync(c => c.Name == card.Name))
+            {
+                return BadRequest();
+            }
+
+            var dbCard = new Card
+            {
+                Name = card.Name,
+                YgoProId = card.Id,
+                CardType = card.Type,
+                MonsterType = card.Race,
+                MonsterAttribute = card.Attribute,
+                MonsterLevel = card.Level,
+                MonsterAttack = card.Atk,
+                MonsterDefense = card.Def,
+                Description = card.Desc
+                //Image = card.Image
+            };
+
+            _context.Cards.Add(dbCard);
+            await _context.SaveChangesAsync();
+
+            return card;
+        }
+    }
+}
