58575f119348cd5718ba889ba0da4f9440684184
Laevahteinn <cade.haynes@gmail.com>
2026-09-06 17:04:14 +1000

    Partial User & CollectionEntry progress

---
+++ YGOCM_BACKEND/Controllers/CollectionController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using YGOCM_BACKEND.Entities;
+
+namespace YGOCM_BACKEND.Controllers
+{
+    // This controller is for managing collection entries
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CollectionController : ControllerBase
+    {
+        // Database reference
+        readonly AppDbContext _context;
+
+        public CollectionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // HTTP Calls
+        [HttpGet("user/{id}")] // GET a specific user's collection entry from the database by their id
+        public async Task<ActionResult<IEnumerable<CollectionEntry>>> GetCollectionByUserId(int id)
+        {
+            return await _context.CollectionEntries.Where(c => c.UserId == id).ToListAsync();
+        }
+
+        [HttpPost] // POST a new collection entry to the database
+        public async Task<ActionResult<CollectionEntry>> PostCollectionEntry(int userId, int cardId, int quantity)
+        {
+            if (await _context.Users.FirstOrDefaultAsync(u => u.Id == userId) == null)
+            {
+                return NotFound("User not found");
+            }
+
+            if (await _context.Cards.FirstOrDefaultAsync(c => c.Id == cardId) == null)
+            {
+                return NotFound("Card not found");
+            }
+
+            var entry = new CollectionEntry
+            {
+                UserId = userId,
+                CardId = cardId,
+                Quantity = quantity
+            };
+            
+            _context.CollectionEntries.Add(entry);
+            await _context.SaveChangesAsync();
+
+            return entry;
+        }
+    }
+}
---
+++ YGOCM_BACKEND/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using YGOCM_BACKEND.Entities;
+
+namespace YGOCM_BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // HTTP Calls
+
+    }
+}
