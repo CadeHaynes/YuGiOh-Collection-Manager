aa114813748cbcae098a3d67338083310396ceac
Laevahteinn <cade.haynes@gmail.com>
2026-09-18 16:06:11 +1000

    Authenication partial implementation using Identity

--- YGOCM_BACKEND/Program.cs
+++ YGOCM_BACKEND/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 using YGOCM_BACKEND;
 using YGOCM_BACKEND.Services;
 
@@ -9,12 +11,19 @@ builder.Services.AddControllers();
 builder.Services.AddHttpClient<YgoProDeckService>();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("YGOCM_DB")));
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("YGOCM_DB")));
+
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AuthDbContext>();
 
 // Swagger implementation
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
